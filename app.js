ccbb905233cb3b62ac41da4fb71b2cc7d5aab8e7
jajangaf <radhi.vendor.pasti@pegadaian.co.id>
2025-09-19 08:37:06 +0700

    belajar response dan request server

---
+++ app.js
@@ -0,0 +1,12 @@
+const http = require('http');
+const port = 3000;
+const server = http.createServer((req, res)=>{
+    res.writeHead(200,{
+        'content-type': 'text/html',
+    });
+res.write('Hello World');
+res.end();
+});
+server.listen(port,()=>{
+    console.log(`Listening is Listening on port ${port}..`);
+});
